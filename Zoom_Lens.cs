10a2ed5a1d7bdb0ad05f1bb81a527b19eb8a6dde
Malthbern <grantjackson95@gmail.com>
2024-12-08 08:04:39 -0600

    Updated with download link

--- Zoom_Lens.cs
+++ Zoom_Lens.cs
@@ -14,8 +14,8 @@ namespace Zoom_Lens
         public const string Description = "Adds a slider to the side of the camera to allow zooming without opening the advance settings.";
         public const string Author = "Malthbern";
         public const string Company = null;
-        public const string Version = "0.1.1";
-        public const string DownloadLink = "";
+        public const string Version = "0.1.2";
+        public const string DownloadLink = "https://github.com/Malthbern/Zoom_Lens/releases";
     }
     
     public class LensMain : MelonMod
