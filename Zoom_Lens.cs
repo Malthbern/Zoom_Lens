87385df05b42266f2c0cf4956c96479989f0c046
Malthbern <grantjackson95@gmail.com>
2024-12-08 08:00:36 -0600

    version incriment
    forgot to increment for release

--- Zoom_Lens.cs
+++ Zoom_Lens.cs
@@ -14,7 +14,7 @@ namespace Zoom_Lens
         public const string Description = "Adds a slider to the side of the camera to allow zooming without opening the advance settings.";
         public const string Author = "Malthbern";
         public const string Company = null;
-        public const string Version = "0.0.1";
+        public const string Version = "0.1.1";
         public const string DownloadLink = "";
     }
     
