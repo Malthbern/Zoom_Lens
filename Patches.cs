900f535525c47c32096474ccbb457efe7d11c48d
Malthbern <61102919+Malthbern@users.noreply.github.com>
2025-09-04 05:29:37 -0500

    Revert "Mod now checks for game version 2025r180 as stable version and added patches for 2025r181 Nightly"
    This reverts commit 3011c98cc82732caeea8b42fc973ef3e535206ed.

--- Patches.cs
+++ Patches.cs
@@ -9,8 +9,6 @@ namespace Zoom_Lens
         public static GameObject Obj = null;
 
         private static readonly Vector3 _offset = new Vector3(65f, 0, 0);
-        private static readonly Vector3 _nightlyoffset = new Vector3(180f, 0, 0);
-        private static readonly Vector3 _nightlyscale = new Vector3(0.65f, 0.65f, 0.65f);
         
         [HarmonyPostfix]
         [HarmonyPriority(Priority.HigherThanNormal)]
@@ -18,17 +16,7 @@ namespace Zoom_Lens
         public static void AttachLens() // Get camera instance transform to connect our mod to the camera it's self
         {
             Obj = GameObject.Instantiate(Assets.Slider, PortableCamera.Instance.gameObject.transform, false);
-            
-            if(LensMain.IsStable) // Tempoary fix for 2025r181
-            {
-                Obj.transform.localPosition = _offset;
-            }
-            else
-            {
-                Obj.transform.localPosition = _nightlyoffset;
-                Obj.transform.localScale = _nightlyscale;
-            }
-            
+            Obj.transform.localPosition = _offset;
             LensMain.ConnectZoom();
         }
         
